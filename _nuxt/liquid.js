33d54a8db3b457615e132243292650c9631e22c0
Felipe da Silva <felipe-sousa92@live.com>
2025-04-22 15:45:17 -0300

    Update liquid.js

--- _nuxt/liquid.js
+++ _nuxt/liquid.js
@@ -36,6 +36,11 @@ document.addEventListener("DOMContentLoaded", () => {
   let prevTimestamp = Date.now();
 
   const gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.error("WebGL not supported");
+  } else {
+    console.log("WebGL initialized");
+  }
   gl.getExtension("OES_texture_float");
 
   let outputColor, velocity, divergence, pressure;
